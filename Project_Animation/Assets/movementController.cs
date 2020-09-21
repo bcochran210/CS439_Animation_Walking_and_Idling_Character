d860d8134266c196ab4f7d26179e2ebe0ca89de8
bcochran210 <46578870+bcochran210@users.noreply.github.com>
2020-09-21 11:23:32 -0500

    Commited files

---
+++ Project_Animation/Assets/movementController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementController : MonoBehaviour
+{
+    float speed = 4;
+    float rotation_speed = 80;
+    float rotation = 0;
+    float gravity = 8;
+
+    Vector3 move_direction = Vector3.zero;
+
+    CharacterController controller;
+    Animator anim;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(controller.isGrounded)
+        {
+            if(Input.GetKey(KeyCode.W))
+            {
+                anim.SetInteger("condition", 1);
+                move_direction = new Vector3(0, 0, 1);
+                move_direction *= speed;
+                move_direction = transform.TransformDirection(move_direction);
+            }
+            if(!Input.GetKey(KeyCode.W))
+            {
+                anim.SetInteger("condition", 0);
+                move_direction = new Vector3(0, 0, 0);
+            }
+
+            rotation += Input.GetAxis("Horizontal") * rotation_speed * Time.deltaTime;
+            transform.eulerAngles = new Vector3(0, rotation, 0);
+
+            move_direction.y -= gravity * Time.deltaTime;
+            controller.Move(move_direction * Time.deltaTime);
+        }
+    }
+}
